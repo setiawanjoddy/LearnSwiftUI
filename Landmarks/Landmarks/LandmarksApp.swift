d429ae27c859e583ab2025f439cf37f50c21b5c3
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2021-05-04 13:18:22 +0700

    initial commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Setiawan Joddy on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
